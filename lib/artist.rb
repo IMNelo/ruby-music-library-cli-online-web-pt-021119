7597dca152cbcd2add167b53d02f7e833fc72a05
Agnelo Nelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-04-09 22:46:13 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,7 +8,22 @@ class Artist
   end
 
   def self.all #class method
-    self.all #returns the class variable which contains the collection/instances of artist from @@all = []
+    @@all #returns the class variable which contains the collection/instances of artist from @@all = []
   end
+
+  def self.destroy_all #class method
+    @@all = [] #resets the class variable @@all & returns an empty array
+  end
+
+  def save #instance method
+    @@all << self #shovels every new instance of artist into the @@all = [] collection where it's saved & stored
+  end
+
+  def self.create(name) #class method, passing (name) as an argument
+    new_artist = Artist.new(name) #instantiates a new instance of artist (Note: a 'name' is required by the 'initialize' method above)
+    new_artist.save #calls the 'save method' (defined above) on the instance 'new_artist' just created & saves it to the @@all class variable
+    new_artist
+  end
+
 
 end
